b5082e4e5016775ac33761e7a757fba4e3122810
Francisco Patalagoity <fpatalagoity@hotmail.com>
2016-04-10 12:40:33 -0300

    eliminando carpeta innecesaria

--- TP_Anual_DDS/src/CGP.java
+++
@@ -1,7 +0,0 @@
-
-public class CGP extends POI{
-	public boolean esCercanoA(POI poi, Comuna comuna){
-		return poi.getLatitud()<=comuna.getLatitudMax()&poi.getLatitud()>=comuna.getLatitudMin()
-				&poi.getLongitud()<=comuna.getLongitudMax()&poi.getLongitud()>=comuna.getLongitudMin();
-	}
-}
--- TP_Anual_DDS/src/LibreriaEscolar.java
+++
@@ -1,8 +0,0 @@
-
-public class LibreriaEscolar implements rubro {
-	
-	public boolean esCercanoA(POI poi){
-		return 0.05>=this.calculateDistance(, unPoiLong, otroPoiLat, otroPoiLong)
-	}
-
-}
