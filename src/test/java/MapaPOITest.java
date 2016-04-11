de665b49db8ef10b612ea3f511df84db220f8f62
Luis <josergp94@gmail.com>
2016-04-10 23:50:33 -0300

    Agrego test "Ultra-simple" para crear estructuras de carpetas

---
+++ src/test/java/MapaPOITest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MapaPOITest {
+
+	@Test
+	public void test() {
+		//test ultra simple, solo para crear estructura de carpetas
+		assertTrue(1+1==2);
+	}
+
+}
