57d35c70477fc61dc5f12eae8eb49556e46699f1
garmendia.utn <garmendia.utn@gmail.com>
2016-04-20 07:21:59 -0300

    Se sacan cosas obsoletas

--- TPPOI/src/main/java/ar/edu/TPPOI/CGP.java
+++ TPPOI/src/main/java/ar/edu/TPPOI/CGP.java
@@ -7,18 +7,14 @@ public class CGP extends EmpresaMultiServicios {
 
 	private Polygon comuna;
 
-	public CGP(String unNombre, String unRubro, Polygon unaComuna){
-		this.setNombreRubroComuna(unNombre, unRubro, unaComuna);
+	public CGP(String unNombre, String unRubro, Polygon unaComuna) {
+		this.nombre = unNombre;
+		this.rubro = unRubro;
+		this.comuna = unaComuna;
 	}
 
 	public boolean estasCercaDe(Point unaCoordenada) {
 		return this.comuna.isInside(unaCoordenada);
-	}
-	
-	public void setNombreRubroComuna(String unNombre, String unRubro, Polygon unaComuna){
-		this.nombre = unNombre;
-		this.rubro = unRubro;
-		this.comuna = unaComuna;
 	}
 
 }
--- TPPOI/src/main/java/ar/edu/TPPOI/ParadaDeColectivo.java
+++ TPPOI/src/main/java/ar/edu/TPPOI/ParadaDeColectivo.java
@@ -1,12 +1,9 @@
 package ar.edu.TPPOI;
 
 import java.time.LocalDateTime;
-import java.util.List;
-
 import org.uqbar.geodds.Point;
 
 public class ParadaDeColectivo extends POI {
-	private String linea;
 
 	public ParadaDeColectivo(String unNombre, Point unaCoordenada) {
 		this(unNombre, unaCoordenada, 100);
@@ -16,12 +13,6 @@ public class ParadaDeColectivo extends POI {
 		this.nombre = unNombre;
 		this.coordenada = unaCoordenada;
 		this.radioCercania = unRadioCercania;
-	}
-
-	protected List<String> palabrasClave() {
-		List<String> posiblesPalabrasClaves = super.palabrasClave();
-		posiblesPalabrasClaves.add(this.linea);
-		return posiblesPalabrasClaves;
 	}
 
 	public boolean estaDisponible(LocalDateTime unMomento, Servicio unServicio) {
